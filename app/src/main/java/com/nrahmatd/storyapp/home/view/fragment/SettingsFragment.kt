f060f7c0651384a73d4faaa0c6384b76ac8a50fe
Nur Rahmat Dwi Riyanto <nrahmatd@gmail.com>
2022-10-24 19:22:21 +0700

    Refactor logout and Bugfix Targeting S+ (version 31 and above) requires chucker

--- app/src/main/java/com/nrahmatd/storyapp/home/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/nrahmatd/storyapp/home/view/fragment/SettingsFragment.kt
@@ -6,8 +6,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.nrahmatd.storyapp.R
-import com.nrahmatd.storyapp.authentication.view.AuthenticationActivity
-import com.nrahmatd.storyapp.database.sharedpref.PreferenceManager
+import com.nrahmatd.storyapp.app.GlobalApp
 import com.nrahmatd.storyapp.databinding.FragmentSettingsBinding
 import com.nrahmatd.storyapp.dialog.BottomSheetConfirmDialog
 import com.sagara.klipz.baseview.BaseFragment
@@ -48,14 +47,7 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
             show(fm, "Show")
             listener = object : BottomSheetConfirmDialog.OnClickListener {
                 override fun onYes() {
-                    PreferenceManager.instance.sharedPref.edit().clear().apply()
-                    startActivity(
-                        Intent(
-                            requireActivity(),
-                            AuthenticationActivity::class.java
-                        ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                    )
+                    GlobalApp.instance.logout()
                 }
             }
         }
